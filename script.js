5a5d09c41cd5e4a3da778fc5eafce9ceb74f92a1
ahmadmalik1 <am8929445@gmail.com>
2022-11-19 18:14:32 +0500

    Print any table with JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// creat function
+// creating a function to print its table
 
-function checkLeap(){
-    var input = document.getElementById("year_input").value;
-    // apply if condition of leap year
-    // leap year comes after every 4 year so
-    if(input%4 == 0){
-        document.getElementById("res").innerText 
-        = input + " is Leap year";
-    }else{
-        document.getElementById("res").innerText 
-        = input + " is not Leap year";
+function printtable(){
+    // getting ids from input field and result div
+    var input_number = document.getElementById("table_input").value;
+    var table_number = document.getElementById("table_number");
+    var result = document.getElementById("result");
+
+    // lets apply loop
+    table_number.innerText = input_number;
+    result.innerText = "";
+    for (var i=1; i<=10; i++ ){
+
+        result.innerText += input_number + " x " + i + " = " + input_number*i + "\n";
+
     }
+
 }
